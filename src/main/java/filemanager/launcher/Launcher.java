d176e71daddf3e83e2cc14c4201c78aa2cdce4d0
Yura <yurakatola@gmail.com>
2018-05-29 15:41:28 +0300

    Added possibility to run checking files for specific time and for now. (#3)

--- src/main/java/filemanager/launcher/Launcher.java
+++ src/main/java/filemanager/launcher/Launcher.java
@@ -5,14 +5,11 @@ import com.google.inject.Injector;
 import filemanager.binder.FileServiceBinderModule;
 import filemanager.controller.Application;
 
-import java.time.LocalDateTime;
-
 public class Launcher {
     public static void main(String[] args) {
         Injector guice = Guice.createInjector(new FileServiceBinderModule());
         Application application = guice.getInstance(Application.class);
-        LocalDateTime time = LocalDateTime.of(2018, 5, 23, 6, 0);
-        application.runProgram(LocalDateTime.now());
+        application.runProgram();
     }
 
 }
