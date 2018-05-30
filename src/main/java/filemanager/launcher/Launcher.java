9d49f66a08b3b3cd3caea6864d35bd63c13b3ff2
yuriy <yurakatola@gmail.com>
2018-05-30 18:17:49 +0300

    Added managed process.

--- src/main/java/filemanager/launcher/Launcher.java
+++ src/main/java/filemanager/launcher/Launcher.java
@@ -4,7 +4,9 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import filemanager.binder.FileServiceBinderModule;
 import filemanager.configuration.FileHandlerConfiguration;
+import filemanager.directorytracker.ScheduleFileDirectory;
 import filemanager.directorytracker.TrackerFileDirectory;
+import filemanager.managed.ScheduleFileDirectoryManaged;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
@@ -15,9 +17,11 @@ public class Launcher extends Application<FileHandlerConfiguration> {
 
     @Override
     public void run(FileHandlerConfiguration configuration, Environment environment) {
-        Injector guice = Guice.createInjector(new FileServiceBinderModule());/*
-        FileHandlerApplication application = guice.getInstance(FileHandlerApplication.class);*/
-        TrackerFileDirectory trackerFileDirectory = guice.getInstance(TrackerFileDirectory.class);
+        Injector guice = Guice.createInjector(new FileServiceBinderModule());
+        ScheduleFileDirectory trackerFileDirectory = guice.getInstance(ScheduleFileDirectory.class);
+        ScheduleFileDirectoryManaged managed = new ScheduleFileDirectoryManaged(trackerFileDirectory);
+        environment.lifecycle().manage(managed);
+        /*FileHandlerApplication application = guice.getInstance(FileHandlerApplication.class);*/
         trackerFileDirectory.goThroughToCheckFile();
     }
 }
