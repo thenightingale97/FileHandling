181089a1b36ee853dbb045a6e3cba93af0dc9669
yuriy <yurakatola@gmail.com>
2018-06-01 17:03:26 +0300

    Added clientResource to handle request, and model of request that indicate the time for what need to do check. Also registered new resource.

--- src/main/java/filemanager/launcher/Launcher.java
+++ src/main/java/filemanager/launcher/Launcher.java
@@ -5,6 +5,7 @@ import com.google.inject.Injector;
 import filemanager.binder.FileServiceBinderModule;
 import filemanager.configuration.FileHandlerConfiguration;
 import filemanager.directorytracker.ScheduleFileDirectory;
+import filemanager.resource.ClientResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
@@ -19,6 +20,7 @@ public class Launcher extends Application<FileHandlerConfiguration> {
     public void run(FileHandlerConfiguration configuration, Environment environment) {
         Injector guice = Guice.createInjector(new FileServiceBinderModule(configuration));
         ScheduleFileDirectory trackerFileDirectory = guice.getInstance(ScheduleFileDirectory.class);
+        environment.jersey().register(new ClientResource(trackerFileDirectory));
         environment.lifecycle().scheduledExecutorService("scheduledTracker")
                 .build()
                 .schedule((Runnable) trackerFileDirectory::goThroughToCheckFile, Long.parseLong(configuration.getTimeInterval()), TimeUnit.MINUTES);
